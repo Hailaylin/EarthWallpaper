09760904838794df7e626497e7240f636d247779
HailayLin <hailay@qq.com>
2022-04-25 15:30:31 +0800

    重构，拆分

---
+++ wallpaper.py
@@ -0,0 +1,16 @@
+
+
+class Wallpaper:
+    '''进行壁纸的操作
+    '''
+
+    def generateBackgroundPic():
+        '''生成背景图片
+        
+        '''
+        pass
+
+    def setBackgroundPic():
+        '''设置背景图片
+        '''
+        pass
